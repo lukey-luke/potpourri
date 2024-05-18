97ffa95a28f84d3b65eb0a76ea95194551ebe80a
Luke Short <lukeshortemail@gmail.com>
2024-05-18 11:54:58 -0700

    update evals for testing decline message

--- llmops-automated-testing/README.md
+++ llmops-automated-testing/README.md
@@ -1,5 +1,7 @@
 ## This code is based on the free LLMOps course hosted by CircleCI
  - https://www.deeplearning.ai/short-courses/automated-testing-llmops/
+ - Code is run in jupyter notebooks and CI is run on free instance of CircleCI
+ - https://app.circleci.com/pipelines/github/CircleCI-Learning/llmops-course/4121/workflows/2d5f5395-2aad-4d8d-95da-82afd96046ff/jobs/4464
 
 ## MLOps Automated Testing
 
--- llmops-automated-testing/evals.py
+++ llmops-automated-testing/evals.py
@@ -1,60 +1,59 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.schema.output_parser import StrOutputParser
+from prompts import system_message
 
 def assistant_chain(
-    system_message,
-    human_template="{question}",
-    llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-    output_parser=StrOutputParser()):
-  
-  chat_prompt = ChatPromptTemplate.from_messages([
-      ("system", system_message),
-      ("human", human_template),
-  ])
-  return chat_prompt | llm | output_parser
+        system_message,
+        human_template="{question}",
+        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+        output_parser=StrOutputParser()):
+
+    chat_prompt = ChatPromptTemplate.from_messages([
+        ("system", system_message),
+        ("human", human_template),
+        ])
+    return chat_prompt | llm | output_parser
 
 def eval_expected_words(
-    system_message,
-    question,
-    expected_words,
-    human_template="{question}",
-    llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-    output_parser=StrOutputParser()):
-    
-  assistant = assistant_chain(
-      system_message,
-      human_template,
-      llm,
-      output_parser)
-    
-  
-  answer = assistant.invoke({"question": question})
-    
+        system_message,
+        question,
+        expected_words,
+        human_template="{question}",
+        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+        output_parser=StrOutputParser()):
+
+    # assistant = assistant_chain(
+    #         system_message,
+    #         human_template,
+    #         llm,
+    #         output_parser)
+    assistant = assistant_chain(system_message)
+
+
+    answer = assistant.invoke({"question": question})
+
   print(answer)
-    
+
   assert any(word in answer.lower() \
-             for word in expected_words), \
-    f"Expected the assistant questions to include \
-    '{expected_words}', but it did not"
+          for word in expected_words), \
+          f"Expected the assistant questions to include \
+          '{expected_words}', but it did not"
 
 
 def evaluate_refusal(
-    system_message,
-    question,
-    decline_response,
-    human_template="{question}", 
-    llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
-    output_parser=StrOutputParser()):
-    
-  assistant = assistant_chain(human_template, 
-                              system_message,
-                              llm,
-                              output_parser)
-  
-  answer = assistant.invoke({"question": question})
+        system_message,
+        question,
+        decline_response,
+        human_template="{question}", 
+        llm=ChatOpenAI(model="gpt-3.5-turbo", temperature=0),
+        output_parser=StrOutputParser()):
+
+    assistant = assistant_chain(system_message)
+
+    answer = assistant.invoke({"question": question})
   print(answer)
-  
+
   assert decline_response.lower() in answer.lower(), \
-    f"Expected the bot to decline with \
-    '{decline_response}' got {answer}"
+          f"Expected the bot to decline with \
+          '{decline_response}' got {answer}"
 
